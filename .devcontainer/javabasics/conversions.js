0fe5f3c35ff6ef0edbc6d77b914aaba8306476e4
noorain-code <noorainaziza2@gmail.com>
2025-02-22 19:04:56 +0000

    conversions

---
+++ .devcontainer/javabasics/conversions.js
@@ -0,0 +1,6 @@
+let score="33"
+console.log(typeof score);
+
+let isLoggedIn="0101"
+let booleanloggedin=Boolean(isLoggedIn)
+console.log(booleanloggedin);
---
+++ .devcontainer/javabasics/variables.js
@@ -0,0 +1,6 @@
+const accountId=1457838
+let accountIdEmil="noor@gmail.com"
+var accountPassword="Jaipur"
+let accountstate
+
+console.table([accountId,accountIdEmil,accountPassword,accountstate]);
